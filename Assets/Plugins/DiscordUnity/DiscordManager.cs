c8a14bc4a6c1675f0ca43fed640f20aa34ecc27c
paulboutros <paulboutros436@hotmail.com>
2022-04-20 17:16:47 +0100

    init

---
+++ Assets/Plugins/DiscordUnity/DiscordManager.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using DiscordUnity;
+using DiscordUnity.API;
+using DiscordUnity.State;
+
+public class DiscordManager : MonoBehaviour, IDiscordServerEvents
+{
+    // see  discord  aplliction  / bot  .. token
+    string botToken = "OTY1MzIzNDQ1MzM0MzMxNDEy.YlxhhA.-2K4vW-JNEMci2fFmy7xCsoMINg";
+    public DiscordLogLevel logLevel = DiscordLogLevel.None;
+
+    #region Singleton
+    public static DiscordManager Singleton { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Singleton != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Singleton = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (Singleton == this)
+        {
+            DiscordAPI.UnregisterEventsHandler(this);
+            DiscordAPI.Stop();
+            Singleton = null;
+        }
+    }
+    #endregion
+
+    protected virtual async void Start()
+    {
+        Debug.Log("Starting DiscordUnity ...");
+        DiscordAPI.Logger = new DiscordLogger(logLevel);
+        DiscordAPI.RegisterEventsHandler(this);
+        await DiscordAPI.StartWithBot(botToken);
+        Debug.Log("DiscordUnity started.");
+    }
+
+    private void Update()
+    {
+        DiscordAPI.Update();
+    }
+
+    public void OnServerJoined(DiscordServer server)
+    {
+        server.Channels.Values.FirstOrDefault(x => x.Type == DiscordUnity.Models.ChannelType.GUILD_TEXT)?.CreateMessage("Hello World!", null, null, null, null, null, null);
+    }
+
+    public void OnServerUpdated(DiscordServer server)
+    {
+
+    }
+
+    public void OnServerLeft(DiscordServer server)
+    {
+
+    }
+
+    public void OnServerBan(DiscordServer server, DiscordUser user)
+    {
+
+    }
+
+    public void OnServerUnban(DiscordServer server, DiscordUser user)
+    {
+
+    }
+
+    public void OnServerEmojisUpdated(DiscordServer server, DiscordEmoji[] emojis)
+    {
+
+    }
+
+    public void OnServerMemberJoined(DiscordServer server, DiscordServerMember member)
+    {
+
+    }
+
+    public void OnServerMemberUpdated(DiscordServer server, DiscordServerMember member)
+    {
+
+    }
+
+    public void OnServerMemberLeft(DiscordServer server, DiscordServerMember member)
+    {
+
+    }
+
+    public void OnServerMembersChunk(DiscordServer server, DiscordServerMember[] members, string[] notFound, DiscordPresence[] presences)
+    {
+
+    }
+
+    public void OnServerRoleCreated(DiscordServer server, DiscordRole role)
+    {
+
+    }
+
+    public void OnServerRoleUpdated(DiscordServer server, DiscordRole role)
+    {
+
+    }
+
+    public void OnServerRoleRemove(DiscordServer server, DiscordRole role)
+    {
+
+    }
+
+    #region Logger
+    public enum DiscordLogLevel
+    {
+        None = 0,
+        Error = 1,
+        Warning = 2,
+        Debug = 3
+    }
+
+    private class DiscordLogger : DiscordUnity.ILogger
+    {
+        private readonly DiscordLogLevel level;
+
+        public DiscordLogger(DiscordLogLevel level)
+        {
+            this.level = level;
+        }
+
+        public void Log(string log)
+        {
+            if (level >= DiscordLogLevel.Debug)
+                Debug.Log(log);
+        }
+
+        public void LogWarning(string log)
+        {
+            if (level >= DiscordLogLevel.Warning)
+            {
+                Debug.LogWarning(log);
+            }
+        }
+
+        public void LogError(string log, Exception exception = null)
+        {
+            if (level >= DiscordLogLevel.Error)
+            {
+                Debug.LogError(log);
+                Debug.LogError(exception);
+            }
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/DiscordController.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+ using UnityEngine.Networking;
+ 
+using System.Net;
+ 
+ 
+using System.IO;
+using System;
+  
+
+public class DiscordController : MonoBehaviour
+{
+
+//Git Hub:
+//https://github.com/business-helper/auto-fill-v2/blob/c3ec06a695d56a296cb43fff55c03c335b5a1ee3/src/js/pages/signin.js#L106
+  //authorixaion code  = AcOLG7GJjoNDIXKREG0NT9OozvNK0r
+// client secret X7q0ISLXVZMF9fyRXgRe4bS482LZItSy
+public static string clientIdstring = "965323445334331412";
+public static System.Int64 clientId = 965323445334331412;
+    private static void CreateObject()
+
+
+        
+    { // ser wuli server ID: 947487866622201886
+          string URL = "https://sub.domain.com/objects.json?api_key=123" ;// "https://sub.domain.com/objects.json?api_key=123";
+        string DATA = @"{""object"":{""name"":""Name""}}";
+
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+        request.Method = "POST";
+        request.ContentType = "application/json";
+        request.ContentLength = DATA.Length;
+        StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII);
+        requestWriter.Write(DATA);
+        requestWriter.Close();
+
+        try
+        {
+            WebResponse webResponse = request.GetResponse();
+            Stream webStream = webResponse.GetResponseStream();
+            StreamReader responseReader = new StreamReader(webStream);
+            string response = responseReader.ReadToEnd();
+           Debug.Log(response);
+            responseReader.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("-----------------");
+           Debug.Log(e.Message);
+        }
+
+    }
+
+   
+  string webhook_link =
+        "https://discord.com/api/webhooks/966042841149210695/q2bjyEd72ZFO3o0S2axPn9bCJPIcQsl5mjzOQLA9xBP0gb1cQfMGxTUrWpBHtpxql_Ek";
+        //"https://discord.com/api/webhooks/965571759489318942/hzf5VgPtOkgcMLE-HZjY8bx-_qHk-6DJ-8ePxvxSH3Sxb_QnH5Do8P0UYOsKJVsiGrai";
+    //"https://discord.com/api/webhooks/779882899339739196/ETwncddV1Xar890W59khZn6xvs7PnhzYMGHPm7dPjljpaTyzlgRHpRS-ZXDJehizUjS_";
+    public Discord.Discord discord;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+ // Call 
+    // authorixaion code  = AcOLG7GJjoNDIXKREG0NT9OozvNK0r
+        Application.OpenURL( "https://discord.com/api/oauth2/authorize?client_id=966257659374350346&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code&scope=guilds%20identify" );
+//*******
+
+        return;
+     //  Application.OpenURL("https://discord.com/invite/TZszHsf92c");
+
+
+  CreateObject();
+
+        StartCoroutine(SendWebhook(webhook_link, "Some Message To the Server", (success) =>
+        {
+            if (success)
+                Debug.Log("Message Sent");
+        }));
+        //***********
+
+
+       
+
+         discord = new Discord.Discord(clientId, (System.UInt64)Discord.CreateFlags.Default ); // client ID
+
+
+    
+
+//discord.GetHas
+ //let members =  discord.guilds.get(ID).members();
+
+      var activityManager  =  discord.GetActivityManager();
+      var activity = new Discord.Activity{
+         Details = " on te desert Map",
+         State  = " Playing GAme Alone" 
+       };
+
+        
+      activityManager.UpdateActivity(  activity, (res) => {
+        if ( res == Discord.Result.Ok ){
+          Debug.Log("Discord status set");
+        }else{
+
+            Debug.Log("Discord status failed");
+        }
+
+      });
+
+
+
+       
+       /*
+       activityManager.SendInvite(clientId, Discord.ActivityActionType.Join, "Come play!", (result) =>
+       {
+           if (result == Discord.Result.Ok)
+           {
+               Debug.Log("Successfully joined !");
+           }
+           else
+           {
+               Debug.Log("Failed to join");
+           }
+       });
+       */
+       // testing User info:
+       //**********************************************************************************
+       var userManager = discord.GetUserManager();
+// GetCurrentUser will error until this fires once.
+userManager.OnCurrentUserUpdate += () => {
+  var currentUser = userManager.GetCurrentUser();
+
+   Debug.Log("username => " +currentUser.Username);
+  Debug.Log("ID  => " +currentUser.Id);
+  Debug.Log("Discriminator => " +currentUser.Discriminator);
+  Debug.Log("Avatar => " +currentUser.Avatar);
+
+ 
+   
+   Program.MainX();
+};
+
+ 
+  TestFecthUserData();
+ 
+    }
+
+
+
+    IEnumerator SendWebhook(string link, string message, System.Action<bool> action)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("content", message);
+        using (UnityWebRequest www = UnityWebRequest.Post(link, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+                action(false);
+            }
+            else
+                action(true);
+        }
+    }
+
+
+
+
+    void TestFecthUserData(){
+
+      System.Int64 clientId =  965323445334331412;
+ var discord = new Discord.Discord(clientId, (System.UInt64)Discord.CreateFlags.Default);
+var userManager = discord.GetUserManager();
+  
+
+//var tt = discord.GetUserManager().GetUser(965323445334331412,  (Discord.Result result, ref Discord.User user ) ;
+ 
+userManager.GetUser( clientId  , (Discord.Result result, ref Discord.User user) =>
+{
+  
+  if (result == Discord.Result.Ok)
+  {
+    
+     Debug.Log("user fetched: " +  user.Bot);
+  }
+  else
+  {
+    Debug.Log("user fetch error:  " +  result);
+  }
+});
+
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        return;
+         discord.RunCallbacks();
+    }
+}

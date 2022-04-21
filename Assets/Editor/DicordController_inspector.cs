a6b8961c858259ca9fd26437b5ec81b858f595f3
paulboutros <paulboutros436@hotmail.com>
2022-04-21 13:26:07 +0100

    added custom inspector button and field to post and get

---
+++ Assets/Editor/DicordController_inspector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+ 
+ 
+
+ 
+
+[CustomEditor(typeof(DiscordController))]
+ public class DicordController_inspector : Editor
+{
+
+    
+    public override void OnInspectorGUI()
+    {
+        DiscordController Script = (DiscordController)target;
+       
+       
+    //    DrawDefaultInspector();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("GET:" ,  GUILayout.Width(50));
+        Script.getUrl = GUILayout.TextField(Script.getUrl, GUILayout.Width(800));
+        GUILayout.EndHorizontal();
+        if (GUILayout.Button( "GET" , GUILayout.Width(100), GUILayout.Height(30)))
+        {
+            Script.Button_GetRequest(Script.getUrl);
+        }
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Post:", GUILayout.Width(50));
+        Script.postUrl = GUILayout.TextField(Script.postUrl, GUILayout.Width(800));
+        GUILayout.EndHorizontal();
+        if (GUILayout.Button("POST", GUILayout.Width(100), GUILayout.Height(30)))
+        {
+            Script.Button_postRequest(Script.postUrl);
+        }
+
+
+
+    }
+}

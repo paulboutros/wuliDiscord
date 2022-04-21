e7ca7963a64f067615fbee43386e0a09fd758a48
paulboutros <paulboutros436@hotmail.com>
2022-04-21 11:21:48 +0100

    paul Boutros first commit from VS

--- Assets/Scripts/DiscordController.cs
+++ Assets/Scripts/DiscordController.cs
@@ -10,25 +10,30 @@ using System.Net;
  
 using System.IO;
 using System;
-  
+using System.Threading.Tasks;
 
 public class DiscordController : MonoBehaviour
 {
 
+
+// find it in  Dicord application Auth2
+// client secret :DpCOc0lq40N2KcOSGikVEQofgoF-irAH
 //Git Hub:
 //https://github.com/business-helper/auto-fill-v2/blob/c3ec06a695d56a296cb43fff55c03c335b5a1ee3/src/js/pages/signin.js#L106
   //authorixaion code  = AcOLG7GJjoNDIXKREG0NT9OozvNK0r
+  //                     AcOLG7GJjoNDIXKREG0NT9OozvNK0r
 // client secret X7q0ISLXVZMF9fyRXgRe4bS482LZItSy
+//Discord provided url , you must open the url and press AUTHORIZE
+//https://discord.com/api/oauth2/authorize?client_id=965323445334331412&redirect_uri=http%3A%2F%2F127.0.0.1&response_type=code&scope=guilds%20identify
+ 
 public static string clientIdstring = "965323445334331412";
 public static System.Int64 clientId = 965323445334331412;
-    private static void CreateObject()
-
-
-        
-    { // ser wuli server ID: 947487866622201886
-          string URL = "https://sub.domain.com/objects.json?api_key=123" ;// "https://sub.domain.com/objects.json?api_key=123";
+    private static void Post_objectCreation(){ // ser wuli server ID: 947487866622201886
+ 
+    
+          string URL =  "https://discord.com/oauth2/authorize?client_id=965323445334331412&redirect_uri=http%3A%2F%2F127.0.0.1&response_type=code&scope=guilds%20identify" ;
         string DATA = @"{""object"":{""name"":""Name""}}";
-
+ 
     HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
         request.Method = "POST";
         request.ContentType = "application/json";
@@ -54,8 +59,33 @@ public static System.Int64 clientId = 965323445334331412;
 
     }
 
-   
-  string webhook_link =
+     
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+
+
+
+
+    string webhook_link =
         "https://discord.com/api/webhooks/966042841149210695/q2bjyEd72ZFO3o0S2axPn9bCJPIcQsl5mjzOQLA9xBP0gb1cQfMGxTUrWpBHtpxql_Ek";
         //"https://discord.com/api/webhooks/965571759489318942/hzf5VgPtOkgcMLE-HZjY8bx-_qHk-6DJ-8ePxvxSH3Sxb_QnH5Do8P0UYOsKJVsiGrai";
     //"https://discord.com/api/webhooks/779882899339739196/ETwncddV1Xar890W59khZn6xvs7PnhzYMGHPm7dPjljpaTyzlgRHpRS-ZXDJehizUjS_";
@@ -67,14 +97,14 @@ public static System.Int64 clientId = 965323445334331412;
 
  // Call 
     // authorixaion code  = AcOLG7GJjoNDIXKREG0NT9OozvNK0r
-        Application.OpenURL( "https://discord.com/api/oauth2/authorize?client_id=966257659374350346&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code&scope=guilds%20identify" );
+   //     Application.OpenURL( "https://discord.com/api/oauth2/authorize?client_id=966257659374350346&redirect_uri=https%3A%2F%2Fwww.google.com%2F&response_type=code&scope=guilds%20identify" );
 //*******
-
+    Post_objectCreation();
         return;
      //  Application.OpenURL("https://discord.com/invite/TZszHsf92c");
 
 
-  CreateObject();
+  
 
         StartCoroutine(SendWebhook(webhook_link, "Some Message To the Server", (success) =>
         {
